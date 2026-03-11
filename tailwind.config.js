2de65b43db07a95b6ba9b35c216c1ff65a4099fb
Mayank Sharma <makedy07feb@gmail.com>
2026-03-11 16:47:26 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.html"],
+  content: [
+    "./*.html",
+    "./components/**/*.html"
+  ],
   theme: {
     extend: {},
   },
